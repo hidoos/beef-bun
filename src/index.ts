65ccf53a5b32de20ef6f24d9036c3d60e8c55409
hidoos <sk8huaban@gmail.com>
2024-11-19 21:32:49 +0800

    add zod

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const userSchema = z.object({
+  name: z.string(),
+  age: z.number(),
+})
+
+app.post(
+  '/users/new',
+  zValidator('json', userSchema, (result, c) => {
+    if (!result.success) {
+      return c.text('Invalid!', 400)
+    }
+  }),
+  async (c) => {
+    const user = c.req.valid('json')
+    console.log(user.name) // string
+    console.log(user.age) // number
+  }
+)
+
+app.get("/", (c) => {
+  return c.text('hello hono')
 })
 
 export default app
